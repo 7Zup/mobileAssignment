ccb35c8779ca6376bd9f2efd5366404bbdad9e04
Fabrice Froehly <fabrice.froehly@epitech.eu>
2017-12-26 20:23:46 +0100

    First commit with new project and fresh pods

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'mobileAssignment' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mobileAssignment
+
+  target 'mobileAssignmentTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'mobileAssignmentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ mobileAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobileAssignment
+//
+//  Created by Fabrice Froehly on 24/12/2017.
+//  Copyright © 2017 Fabrice Froehly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
